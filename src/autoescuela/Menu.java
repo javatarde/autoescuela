854495b50948b26a4b8606090fa09040f7374fdd
Formacion <Formacion@10.0.2.15>
2014-10-29 17:23:12 +0100

    Create DiaSemana.java

--- src/autoescuela/Menu.java
+++ src/autoescuela/Menu.java
@@ -3,13 +3,90 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package autoescuela;
+
+import java.util.List;
 
 /**
  *
- * @author Formacion
+ * @author Administracion
  */
-public interface Menu {
-    public Menu ejecutar();
+public class Menu {
+    
+    private Menu siguiente = null;
+    private Menu anterior = null;
+    private List<Opcion> listaOpciones = null;
+
+    public Menu() {
+        // constructor vacio
+    }
+    
+    public Menu(List <Opcion> listaOpciones) {
+        this.listaOpciones = listaOpciones;
+        siguiente = null;
+        anterior = null;
+    }
+    
+    public void mostrarOpciones(){
+        Opcion opcion;
+        for (int i=1; i<=getNumAcciones(); i++) {
+            opcion = getAccion(i-1);
+            System.out.println(i+") "+opcion.getRotulo());
+        }
+    }
+    
+    public Opcion getAccion(int index){
+        if ((index>=0) && (index<getNumAcciones())){
+            return listaOpciones.get(index);
+        }else{
+            return null;
+        }
+    }
+    
+    public int getNumAcciones(){
+        if (listaOpciones != null){
+            return listaOpciones.size();
+        }else{
+            return 0;
+        }
+    }
+
+    public Menu getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Menu siguiente) {
+        this.siguiente = siguiente;
+    }
+
+    public Menu getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Menu anterior) {
+        this.anterior = anterior;
+    }
+    
+    
+    
+    public class Opcion implements Accion<Menu>{
+        private String rotulo;
+        private Accion<Menu> accion;
+
+        public Opcion(String rotulo, Accion accion) {
+            this.rotulo = rotulo;
+            this.accion = accion;
+        }
+
+        @Override
+        public Menu ejecutar() {
+            return accion.ejecutar();
+        }
+
+        public String getRotulo() {
+            return rotulo;
+        }
+        
+    }
+    
 }
